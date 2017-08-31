f17155320d6a0e1ef55d38d0c8e3884ed1440d99
dennismo <dennismoyunfei@gmail.com>
2017-08-31 12:56:33 -0700

    Initial Commit

---
+++ ResizingImages/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ResizingImages
+//
+//  Created by Dennis Mo on 2017-08-31.
+//  Copyright © 2017 Dennis Mo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ResizingImages/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ResizingImages
+//
+//  Created by Dennis Mo on 2017-08-31.
+//  Copyright © 2017 Dennis Mo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
